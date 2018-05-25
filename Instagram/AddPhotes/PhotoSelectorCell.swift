c3f98121150ac7f12629631af1bdf27b57d2a663
takuyaOhmuro <takuyaohmuro@winasNoMini.local>
2018-05-25 10:11:58 +0900

    Fetch Photos Framework

---
+++ Instagram/AddPhotes/PhotoSelectorCell.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoSelectorCell.swift
+//  Instagram
+//
+//  Created by takuyaOhmuro on 2018/05/24.
+//  Copyright © 2018年 takuyaOhmuro. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorCell: UICollectionViewCell {
+    
+    let photoImageView:UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 0, paddingBottom: 0, width: 0, height: 0)
+        backgroundColor = .brown
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

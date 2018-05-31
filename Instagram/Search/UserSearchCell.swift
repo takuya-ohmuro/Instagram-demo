1ece210981be85528df411736c009da65363c5be
takuyaOhmuro <takuyaohmuro@winasNoMini.local>
2018-05-31 12:41:19 +0900

    User Search Controller

---
+++ Instagram/Search/UserSearchCell.swift
@@ -0,0 +1,43 @@
+//
+//  UserSearchCell.swift
+//  Instagram
+//
+//  Created by takuyaOhmuro on 2018/05/31.
+//  Copyright © 2018年 takuyaOhmuro. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchCell: UICollectionViewCell {
+    let profileImageView:CustomImageView =  {
+        let iv = CustomImageView()
+        iv.backgroundColor = .red
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 50 / 2
+        iv.clipsToBounds = true
+        return iv
+    }()
+    let userNameLabel:UILabel = {
+        let label = UILabel()
+        label.text = "UserName"
+        label.font  = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(profileImageView)
+        addSubview(userNameLabel)
+        profileImageView.anchor(top: nil, left: leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingRight: 0, paddingBottom: 0, width: 50, height: 50)
+        profileImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        userNameLabel.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 8, paddingRight: 0, paddingBottom: 0, width: 0, height: 0)
+        let separatorView = UIView()
+        separatorView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        addSubview(separatorView)
+        separatorView.anchor(top: nil, left: userNameLabel.leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 0, paddingBottom: 0, width: 0, height: 0.5)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Instagram/Search/UserSearchController.swift
@@ -0,0 +1,42 @@
+//
+//  UserSearchController.swift
+//  Instagram
+//
+//  Created by takuyaOhmuro on 2018/05/31.
+//  Copyright © 2018年 takuyaOhmuro. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
+    
+    let searchBar:UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Enter username"
+        sb.barTintColor = .gray
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230)
+        return sb
+    }()
+    let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .white
+        navigationController?.navigationBar.addSubview(searchBar)
+        let navBar = navigationController?.navigationBar
+        searchBar.anchor(top: navBar?.topAnchor, left: navBar?.leftAnchor, bottom: navBar?.bottomAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingLeft: 8, paddingRight: -8, paddingBottom: 0, width: 0, height: 0)
+        collectionView?.register(UserSearchCell.self, forCellWithReuseIdentifier: cellId)
+        
+        collectionView?.alwaysBounceVertical = true
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+}
